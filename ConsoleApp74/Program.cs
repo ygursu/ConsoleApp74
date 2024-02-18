9d5072b0396b3ba83efb1faaccfbca41b3da2472
ygursu <yasemingrs@gmail.com>
2024-02-18 23:03:57 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp74/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilensayininhangiguneait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Lütfen bir sayı giriniz");
+            int sayi = Int32.Parse(Console.ReadLine());
+
+            if (sayi == 1)
+            {
+                Console.WriteLine("Pazartesi");
+            }
+            else if (sayi == 2)
+            {
+                Console.WriteLine("Salı");
+            }
+            else if (sayi == 3)
+            {
+                Console.WriteLine("Çarşamba");
+            }
+            else if (sayi == 4)
+            {
+                Console.WriteLine("Perşembe");
+            }
+            else if (sayi == 5)
+            {
+                Console.WriteLine("Cuma");
+            }
+            else if (sayi == 6)
+            {
+                Console.WriteLine("Cumartesi");
+            }
+            else if (sayi == 7)
+            {
+                Console.WriteLine("Pazar");
+            }
+            else
+            {
+                Console.WriteLine("Böyle bir gün yok");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
